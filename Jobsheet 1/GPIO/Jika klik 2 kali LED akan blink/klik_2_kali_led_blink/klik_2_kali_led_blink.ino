eff1c379806cfbfa3d2359a364c45afc6683ea4c
ghinazhafirah <151806874+ghinazhafirah@users.noreply.github.com>
2023-11-23 11:16:15 +0700

    code dan video

---
+++ Jobsheet 1/GPIO/Jika klik 2 kali LED akan blink/klik_2_kali_led_blink/klik_2_kali_led_blink.ino
@@ -0,0 +1,55 @@
+  const int buttonPin = 4; // the number of the pushbutton pin
+  const int buttonPin1 = 18;
+  const int ledPin = 5; // the number of the LED pin
+  const int ledPin1 = 19;
+  
+  // variable for storing the pushbutton status 
+  int a = 0;
+  int b = 0;
+  int buttonState = 0;
+  int button_state = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello, ESP32!");
+  // initialize the pushbutton pin as an input
+  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin1, INPUT);
+  // initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+
+  }
+
+void loop() {
+ buttonState = digitalRead(buttonPin);
+ //Serial.println(buttonState);
+ if (buttonState == HIGH) {
+ // turn LED on
+ digitalWrite(ledPin, HIGH);
+ } else {
+ digitalWrite(ledPin, LOW);
+ }
+
+button_state = digitalRead(buttonPin1);
+
+if (button_state == HIGH){
+ b=a++;
+ delay(200);
+  
+}
+Serial.println(b);
+if (b == 1){
+  
+  digitalWrite(ledPin1, HIGH);
+  delay(300);
+  digitalWrite(ledPin1, LOW);
+  delay(300);
+  b = 0;
+  a = 0;
+}
+
+  delay(10); // this speeds up the simulation
+}
